923f82ae54f5cc5137036ec93b9eac5c6592969e
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-04-05 01:36:54 +0100

    Fix for legacy LegacyBindAttrNode has no get method

--- test-support/helpers/201-created/utils/each-view.js
+++ test-support/helpers/201-created/utils/each-view.js
@@ -7,7 +7,8 @@ export default function(app, callback){
   });
 
   var liveViews = views.filter(function(view) {
-    return (!view.get('isDestroyed') && !view.get('isDestroying'));
+    return (view.get && // fix for LegacyBindAttrNode has no get method
+            !view.get('isDestroyed') && !view.get('isDestroying'));
   });
 
   liveViews.forEach(callback); // views contains every view in the page
