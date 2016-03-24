0fb7bf3cd5b08e45eed3c178a53702d8fbf6c7b2
Alex DiLiberto <alex@alexdiliberto.com>
2016-03-24 17:23:57 -0400

    fix(test): fixing travis error for 1.11.3 and 1.12.1

--- tests/dummy/app/components/click-component.js
+++ tests/dummy/app/components/click-component.js
@@ -6,7 +6,10 @@ export default Ember.Component.extend({
   classNames: ['click-component'],
   text: 'click me',
   didClick: false,
-  doClick() {
-    this.toggleProperty('didClick');
+  
+  actions: {
+	  doClick() {
+	    this.toggleProperty('didClick');
+	  }
   }
 });
